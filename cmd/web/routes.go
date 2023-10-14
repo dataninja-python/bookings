6f085165e653ee069e52c74070c06de96c107c77
dataninja-python <the.coders.dao@gmail.com>
2023-10-14 14:27:29 -0400

    something is wrong with template code

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -27,6 +27,13 @@ func routes(app *config.AppConfig) http.Handler {
 	// listen for the routes
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/contact", handlers.Repo.Contact)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+
+	mux.Get("/search-availability", handlers.Repo.Availability)
+
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
 
 	// get and store the image file from the static directory
 	fileServer := http.FileServer(http.Dir("./static/"))
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -68,3 +68,33 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 		StringMap: stringMap,
 	})
 }
+
+// Reservation is the handler for the generals quarters room page
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	pkgAnnouncer()
+	render.RenderTemplate(w, "make-reservation.page.tmpl", &models.TemplateData{})
+}
+
+// Generals is the handler for the generals quarters room page
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	pkgAnnouncer()
+	render.RenderTemplate(w, "generals.page.tmpl", &models.TemplateData{})
+}
+
+// Majors is the handler for the majors suite room page
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	pkgAnnouncer()
+	render.RenderTemplate(w, "majors.page.tmpl", &models.TemplateData{})
+}
+
+// Availability is the handler for the majors suite room page
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	pkgAnnouncer()
+	render.RenderTemplate(w, "search-availability.page.tmpl", &models.TemplateData{})
+}
+
+// Contact is the handler for the contact page
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	pkgAnnouncer()
+	render.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
+}
