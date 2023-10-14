63c79df148a394159fe61ff5261b6dd11232eba3
dataninja-python <the.coders.dao@gmail.com>
2023-10-13 22:10:23 -0400

    adding updated html files

---
+++ static/js/main.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", function () {
+    main();
+});
+//
+var main = function () {
+    // 👇️ const input: HTMLInputElement | null
+    var input = document.getElementById("start_date");
+    var value = input === null || input === void 0 ? void 0 : input.value;
+    console.log(value); // 👉️ "Initial value"
+    if (input != null) {
+        console.log(input.value); // 👉️ "Initial value"
+    }
+    input === null || input === void 0 ? void 0 : input.addEventListener("input", function (event) {
+        var target = event.target;
+        console.log(target.value);
+        displayToScreen(target.value);
+    });
+    var displayToScreen = function (val) {
+        var outputDisplay = document.getElementById("values");
+        outputDisplay.textContent = val;
+    };
+};
---
+++ static/js/main.ts
@@ -0,0 +1,27 @@
+window.addEventListener("load", () => {
+    main()
+})
+
+//
+const main = () => {
+    // 👇️ const input: HTMLInputElement | null
+    const input = document.getElementById("start_date") as HTMLInputElement | null;
+
+    const value = input?.value;
+    console.log(value) // 👉️ "Initial value"
+
+    if (input != null) {
+        console.log(input.value); // 👉️ "Initial value"
+    }
+
+    input?.addEventListener("input", function (event) {
+        const target = event.target as HTMLInputElement;
+        console.log(target.value);
+        displayToScreen(target.value);
+    });
+
+    const displayToScreen = (val) => {
+        let outputDisplay = document.getElementById("values");
+        outputDisplay.textContent = val;
+    }
+}
